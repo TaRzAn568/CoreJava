fc0ed97ccb0c4df33a21b95aed983dd9808de220
VedPrakash <ved.prakash@saviynt.com>
2019-09-02 01:00:00 +0530

    StringSumUsingSplit

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ class Main{
     public static void main (String[] args)throws IOException {
         ConvertToInteger obj = new ConvertToInteger();
         obj.convertToInteger();
+        obj.sumOfStringUsingSplit();
     }
 }
 //1abc2x30yz67
@@ -49,6 +50,29 @@ class ConvertToInteger {
             System.out.println(x);
         }
 
+    }
+
+    public void sumOfStringUsingSplit()throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t =  Integer.parseInt(br.readLine());
+        List<BigInteger> resultList = new ArrayList<BigInteger>();
+        while(t>0){
+            String s = br.readLine();
+            int i = 0;
+            int j = i;
+            BigInteger bg = new BigInteger("0");
+            String a[] = s.split("\\D");
+            for(String x : a){
+                if(!x.isEmpty()){
+                    bg = bg.add(new BigInteger(x));
+                }
+            }
+            resultList.add(bg);
+            t--;
+        }
+        for(BigInteger x : resultList){
+            System.out.println(x);
+        }
     }
 }
 
